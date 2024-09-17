07abbb8dc7522c965d4eeaae74673f4ba184c1ac
Ana Flavia Moreira <anaflaviamoreirasouzaa@gmail.com>
2024-09-17 20:29:01 -0300

    Inicio de novo

---
+++ src/Cabecalho.java
@@ -0,0 +1,10 @@
+public class Cabecalho {
+    public static void escrevacabecalho () {
+        System.out.println("Faculdade: Unifan");
+        System.out.println("Discente: Ana Flavia Moreira Souza");
+        System.out.println("Docente: Brenno Pimenta");
+        System.out.println("Turma: ESN2 - A");
+        System.out.println("Bem vindo ao quiz de Gráficos");
+        System.out.println("Utilize para estudar para Estatística");
+    }
+}
