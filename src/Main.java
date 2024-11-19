33cc4d43077dde2dcac878cc257c68beb6b144f7
Ana Flavia <anaflaviamoreirasouza@gmail.com>
2024-11-19 20:01:14 -0300

    Ajeitando

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Cabecalho.escrevacabecalho();
 
+        Cabecalho.escrevacabecalho();
         Perguntas.main(new String[]{});
     }
 }
